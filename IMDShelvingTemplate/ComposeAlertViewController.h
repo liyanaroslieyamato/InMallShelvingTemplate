c83c79a46a9678c09a08c11408da211c27b9dcea
liyanaroslieyamato <liyana@yamatoasia.com>
2016-08-26 14:57:37 +0800

    Add Compose Alert to Loading Bay page

---
+++ IMDShelvingTemplate/ComposeAlertViewController.h
@@ -0,0 +1,18 @@
+//
+//  ComposeAlertViewController.h
+//  IMDShelvingTemplate
+//
+//  Created by Liyana Roslie on 26/8/16.
+//  Copyright © 2016 Liyana Roslie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ComposeAlertViewController : UIViewController <UICollectionViewDataSource,UICollectionViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIButton *unitBtnOutlet;
+@property (strong, nonatomic) IBOutlet UICollectionView *collectionView;
+
+- (IBAction)selectUnit:(id)sender;
+
+@end
